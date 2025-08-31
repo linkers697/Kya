22694f62e97bda7905d01e1086b166a37a3131ba
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2025-08-31 17:57:36 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -28,13 +28,9 @@ def private_panel(_):
             )
         ],
         [
-            InlineKeyboardButton(
-                text="Help",
-                callback_data="settings_back_helper",
-
-            InlineKeyboardButton(text="New Feacture, url=f"https://telegra.ph/FFcg-08-31"),
-        ],
-        
+            InlineKeyboardButton(text="Help", callback_data="settings_back_helper",
+            InlineKeyboardButton(text="New Feacture", url=f"https://telegra.ph/New-Features-08-31"),
+        ], 
         [
             InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG"),
         ],
