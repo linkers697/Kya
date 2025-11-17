fa9c516336ad5be02ddc28c3623ad9be8d9f7f2d
pritamarya2627-gif <pritamarya2627@gmail.com>
2025-11-17 15:21:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ API_HASH = getenv("API_HASH")
 
 
 YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## E.G https://yt.okflix.
-YT_API_KEY = getenv("YT_API_KEY", 'xbit_NJOW4LF65OQ6UMNAYT9YLL')#+&&_
+YT_API_KEY = getenv("YT_API_KEY", "xbit_Z40UQMGPMUSLT99508XYKG")#+&&_
 
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
