0804a580df85b1de24235cc0ef561baa611e529b
pritamarya2627-gif <pritamarya2627@gmail.com>
2025-11-21 13:17:50 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -68,7 +68,7 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
 
         # [
-        InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"), 
+        #     InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"), 
         # ],
     ]
     return buttons
