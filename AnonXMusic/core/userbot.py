a9ff582331460100c3c5bd727c26f11fffba71c8
linkers697 <music3fhhgyhfgh@gmail.com>
2026-01-04 17:50:31 +0530

    Update userbot.py

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("Lavbots")
-                await self.one.join_chat("TgMusicBots")
+                await self.one.join_chat("AuraVisual")
+                await self.one.join_chat("friends_hindi_chatting_grp")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("Lavbots")
-                await self.two.join_chat("TgMusicBots")
+                await self.two.join_chat("AuraVisual")
+                await self.two.join_chat("friends_hindi_chatting_grp")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("Lavbots")
-                await self.three.join_chat("TgMusicBots")
+                await self.three.join_chat("AuraVisual")
+                await self.three.join_chat("friends_hindi_chatting_grp")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("Lavbots")
-                await self.four.join_chat("TgMusicBots")
+                await self.four.join_chat("AuraVisual")
+                await self.four.join_chat("friends_hindi_chatting_grp")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("Lavbots")
-                await self.five.join_chat("TgMusicBots")
+                await self.five.join_chat("AuraVisual")
+                await self.five.join_chat("friends_hindi_chatting_grp")
             except:
                 pass
             assistants.append(5)
